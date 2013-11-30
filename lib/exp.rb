4de1beef1baae47b91c88e8cfe5c5c89257d407e
michael d. towle <gametheoretic@gmail.com>
2013-11-30 14:11:39 -0500

    req. user to set EXP_DIR

--- lib/exp.rb
+++ lib/exp.rb
@@ -4,9 +4,14 @@ class Object
 
 	def exp 
 		begin
-			print "quick export to: "
-			fname = gets.strip
-			f = open(fname, "w")
+			if not EXP_DIR
+				print "EXP_DIR: "
+				EXP_DIR = gets
+			end
+		
+			print "export filename: "
+			fname = gets
+			f = open("#{EXP_DIR}/#{fname.strip}", "w")
 			f.write(JSON.generate(self))
 			f.close
 			puts "Success; \"#{fname}\" closed."
